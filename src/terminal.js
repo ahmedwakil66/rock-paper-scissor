700d89e02e3d4f8e1cac4c093d6fd826347b0335
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-12 00:46:46 +0600

    removed redundant comments

--- src/terminal.js
+++ src/terminal.js
@@ -26,19 +26,3 @@ class Terminal {
 }
 
 export default Terminal;
-
-// console.log('Starting the game ...');
-
-// const r = rl.question('Enter your first name: ', async(input) => {
-//     console.log(input);
-//     rl.promt
-
-//     await new Promise(resolve => setTimeout(() => {resolve()}, 2000));
-
-//     rl.question('Enter your last name: ', (input2) => {
-//         console.log(input2);
-//         console.log(input + " " + input2);
-
-//         rl.close()
-//     })
-// })
