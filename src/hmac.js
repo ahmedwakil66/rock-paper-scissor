b18ada9f5e0d31590a80dfb044a0f32d6743e765
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-11 23:24:47 +0600

    HMAC generation logic

---
+++ src/hmac.js
@@ -0,0 +1,19 @@
+import crypto from "node:crypto";
+
+class HMAC {
+  randomKey(size) {
+    return crypto.randomBytes(size || 32);
+  }
+
+  getStringKey(size) {
+    return this.randomKey(size || 32).toString("hex");
+  }
+
+  createHmac(message, key, alg) {
+    const hmac = crypto.createHmac(alg || "sha256", key);
+    hmac.update(message);
+    return hmac.digest("hex");
+  }
+}
+
+export default HMAC;
